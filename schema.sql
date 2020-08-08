497dbb9d62f4c6970552549591ba750e94d8b377
Tuomas <tietotuomas@gmail.com>
2020-08-09 01:33:46 +0300

    Create content
    File was empty.

---
+++ schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE quizzes (
+    id SERIAL PRIMARY KEY, 
+    topic TEXT
+);
+CREATE TABLE questions (
+    id SERIAL PRIMARY KEY, 
+    content TEXT,
+    quiz_id INTEGER REFERENCES quizzes
+);
+CREATE TABLE answers (
+    id SERIAL PRIMARY KEY, 
+    content TEXT,
+    question_id INTEGER REFERENCES questions,
+    correct BOOLEAN
+);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    admin BOOLEAN
+);
+CREATE TABLE user_answers ( 
+    user_id INTEGER REFERENCES users,
+    answer_id INTEGER REFERENCES answers
+);
