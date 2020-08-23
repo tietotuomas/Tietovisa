8b13e86f6299ff2241b932bebf2ccd7e8432226f
Tuomas <tietotuomas@gmail.com>
2020-08-23 11:09:48 +0300

    Edit feedback phrases

--- utilities.py
+++ utilities.py
@@ -3,12 +3,12 @@ from random import choice
 def get_feedback(points):
     message_list = 0
     feedback_limit = [0, 0.2, 0.4, 0.6, 0.8, 1]
-    feedback = [["Hups... vaikeita kysymyksiä?", "...oliko keskittyminen hukassa?"], \
+    feedback = [["... vaikeita kysymyksiä?", "...oliko keskittyminen hukassa?"], \
         ["No... olisihan se huonomminkin voinut mennä.", "Ei mennyt kuin Strömsössä."], \
-        ["Ei huono, kyllä tällä luokalta pääsee.", "Ihan kohtuullinen esitys."], \
+        ["Ei huono, ehkä tällä luokalta päässee.", "Ihan kohtuullinen esitys."], \
         ["Vähintäänkin tyydyttävä tulos!", "Ei hassummin!", "Tavattoman kelpo tulos!"], \
         ["Wau, kiitettävä suoritus!", "Fantastista tekemistä!", "Upeaa, aihepiiri selvästi hallussa!"], \
-        ["No, terve! Ethän vain huijannut?", "Täydellistä!", "Mestarisuoritus!"]]
+        ["No, terve! Ethän vain huijannut?", "Suorastaan timanttista!", "Mestarisuoritus!"]]
     while message_list < len(feedback) and points > feedback_limit[message_list]:
         message_list += 1
     return choice(feedback[message_list])
